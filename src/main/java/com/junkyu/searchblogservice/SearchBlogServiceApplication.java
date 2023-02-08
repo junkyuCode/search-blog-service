801d50be5733ea8302208e07a7cd8389cfdfe91f
박준규 <junkyu.park@kurlycorp.com>
2023-02-08 19:47:46 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/com/junkyu/searchblogservice/SearchBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.junkyu.searchblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchBlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchBlogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junkyu/searchblogservice/SearchBlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junkyu.searchblogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchBlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
